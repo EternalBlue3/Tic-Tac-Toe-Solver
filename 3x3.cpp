010f2181f14b0fe7c1d4f1fcb1da43ff50d34885
James Key <100178397+EternalBlue3@users.noreply.github.com>
2023-10-06 20:39:02 -0400

    Update 3x3.cpp

--- 3x3.cpp
+++ 3x3.cpp
@@ -152,12 +152,6 @@ tuple<int, int> solve(vector<int> gameboard, int player, int depth) {
             best_score = score;
             best_move = move;
         }
-        
-        alpha = max(alpha, score);
-                
-        if (alpha >= beta) {
-            break;
-        }
     }
     
     return make_tuple(best_move, best_score);
